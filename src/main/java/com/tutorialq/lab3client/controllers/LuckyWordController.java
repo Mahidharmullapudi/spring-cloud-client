888b70e45c02e26090bf292d6162510ae005415f
Mahidhar <mahidhar_mullapudi@outlook.com>
2018-03-02 23:26:31 -0500

    Initial Commit

---
+++ src/main/java/com/tutorialq/lab3client/controllers/LuckyWordController.java
@@ -0,0 +1,16 @@
+package com.tutorialq.lab3client.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LuckyWordController {
+
+    @Value("${name}") String luckyWord;
+
+    @GetMapping("/lucky-word")
+    public String showLuckyWord() {
+        return "The lucky word is: " + luckyWord;
+    }
+}
